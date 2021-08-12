fae06ada192b871490e6c4d7ce739611182e3006
ezequielg1989 <ezequiel.ganzero1@gmail.com>
2021-08-11 22:15:16 -0300

    repo git y heroku

---
+++ app.js
@@ -0,0 +1,13 @@
+const express= require("express");
+const app= express()
+const path= require("path");
+const puerto= process.env.PORT || 3000
+
+app.use(express.static(path.join(__dirname, "./public")));
+app.get("/", (req, res) => {
+  res.sendFile(path.join(__dirname, "./view/index.html"));
+});
+
+app.listen(puerto, () => {
+  console.log('Server is running on PORT:'+ puerto);
+});
