a0600ce5c0026610da06f94d64ab49d251907937
ezequielg1989 <ezequiel.ganzero1@gmail.com>
2021-08-17 13:21:40 -0300

    subiendo a heroku responsive

--- app.js
+++ app.js
@@ -5,7 +5,7 @@ app.use(express.static(path.join(__dirname, "./public")));
 app.get("/", (req, res) => {
   res.sendFile(path.join(__dirname, "/index.html"));
 });
-const puerto= process.env.PORT || 3000
+const puerto= process.env.PORT || 3001
 
 app.listen(puerto, () => {
   console.log("Server is running on PORT :"+puerto);
